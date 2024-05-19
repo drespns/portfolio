c22da97447707991ac3c51a6ba68d406bdf375b0
Daniel Rguez Espinosa <drespns@gmail.com>
2024-05-19 23:24:06 +0100

    States pending for the future to be updated

---
+++ link_bio_web/states/technologies.py
@@ -0,0 +1,16 @@
+from typing import List
+
+import reflex as rx
+
+class SourceForeachState(rx.State):
+    src: List[str] = [
+        "/icons/icons8-html-5-100.png",
+        "/icons/icons8-css3-100.png",
+        "/icons/icons/icons8-javascript-128.ico",
+        "/icons/icons8-python-100.png",
+        "/reflex_banner.png",
+        "/icons/icons8-react-100.png",
+        "/icons/icons8-tailwind-css-100.png",
+        "/icons/icons8-tableau-software-96.png",
+        "/icons/icons8-css3-100.png",
+    ]
