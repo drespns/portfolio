c232966e152f68d2ea1ff1dc064d777d6307583b
Daniel Rguez Espinosa <drespns@gmail.com>
2024-05-19 23:22:51 +0100

    Adding color styles
    Some classes - Color and TextColor

---
+++ link_bio_web/styles/colors.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class Color(Enum):
+    PRIMARY = "#3B84C9"
+    SECONDARY = "#3378b4"
+    BACKGROUND = "#142B41"
+    CONTENT = "#171F26"
+    SHADOW = "#9039C9"
+
+class TextColor(Enum):
+    HEADER = "#F1F2F4"
+    BODY = "#C3C7CB"
+    FOOTER = "#A3ABB2"
--- link_bio_web/styles/styles.py
+++ link_bio_web/styles/styles.py
@@ -2,31 +2,78 @@ from enum import Enum
 
 import reflex as rx
 
-# Constants:
-MAX_WIDTH="600px"
+from .colors import Color, TextColor
+
 
 # Sizes:
 class Size(Enum):
-    SMALL = "0.5em"
+    ZERO = "0 !important"
+    SMALL = "0.5em" # 1em <- tamaño fuente de la app.
     MEDIUM = "0.75em"
     DEFAULT = "1em"
-    BIG = "2em"
+    BIG = "1.5em"
+    VERY_BIG = "2em"
+    ULTRA_BIG = "2.5em"
+    HUMONGOUS = "3em"
+    GARGANTUAN = "3em"
+
+
+# Constants:
+MAX_WIDTH="600px"
 
 # Global styles:
-"""styles = {
-    rx.chakra.Button
-}"""
+BASE_STYLE = { # NOTE: solo se toman de aquí las propiedades CSS.
+    "background_color": Color.BACKGROUND.value,
+    rx.avatar: {
+        #"size": "8",
+        #fallback='DR',
+        # high_contrast=True,
+        # variant='solid',
+        #"radius":"full",
+        "border":"5px solid",
+        "border_color": Color.SECONDARY.value,
+        "box_shadow": f"{Color.SHADOW.value} 0 5px 5px"
+    },
+    rx.button: {
+        "background_color": Color.CONTENT.value,
+        "color": TextColor.HEADER.value,
+        "width": "100%",
+        "height": "100%",
+        "cursor": "pointer",
+
+        "_hover": {
+            "background_color": Color.SECONDARY.value,
+        }
+    },
+    rx.link: {
+        "_hover": {
+            "border_color": Color.SHADOW.value,
+        }
+    },
+    rx.box: {
+        "color": TextColor.BODY.value,
+        "font_size": Size.MEDIUM.value
+    },
+    rx.text: {
+        "color": TextColor.BODY.value
+    }
+}
+
+# Other styles:
 
 title_style = dict(
     size="lg",
     width="100%",
     padding_top=Size.DEFAULT.value,
+    color=TextColor.HEADER.value
 )
 
 button_title_style = dict(
     font_size=Size.DEFAULT.value,
+    color=TextColor.HEADER.value
 )
 
 button_subtitle_style = dict(
     font_size=Size.MEDIUM.value,
+    color=TextColor.BODY.value
 )
